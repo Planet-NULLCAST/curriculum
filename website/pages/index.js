077ae1fcda86ba072990c56fcc05e5337e3838e1
ASEEMMUHAMMAD A. R <azm@pop-os.localdomain>
2021-04-19 16:20:17 +0530

    Feat: added editor to dashboard
    A new codemirror editor is added to our learning dashboard

--- website/pages/index.js
+++ website/pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 
 import Layout from '../component/layout/Layout.jsx';
 import Description from '../component/layout/common/Discription.jsx';
-import Monoco from '../component/editor/Editor.js';
 
 export default function Home() {
   return (
