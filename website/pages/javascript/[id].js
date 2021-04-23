046da77e04a416d22b157aeca0ae85df178c8e0f
akhilalekha <akhila160@gmail.com>
2021-04-23 11:37:50 +0530

    fix: remove console.log

--- website/pages/javascript/[id].js
+++ website/pages/javascript/[id].js
@@ -25,7 +25,6 @@ export async function getStaticProps({ params }) {
     }
 }
 export default function Javascript({ postData }) {
-    console.log(postData);
     return (
         <Layout>
             <Head>
