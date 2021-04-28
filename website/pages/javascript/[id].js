5ca0bf6c7172a62de63c9e2df2153168eaebcd63
akhilalekha <akhila160@gmail.com>
2021-04-28 16:26:53 +0530

    feat: create an output component to display the test results

--- website/pages/javascript/[id].js
+++ website/pages/javascript/[id].js
@@ -1,59 +1,87 @@
-import Head from 'next/head';
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 
-import Layout from '../../component/layout/Layout'
-import { getAllPostIds, getPostData } from '../../lib/jslist'
+import Layout from "../../component/layout/Layout";
+import { getAllPostIds, getPostData } from "../../lib/jslist";
 
-import { ArrowCircleLeftIcon, ArrowCircleRightIcon } from '@heroicons/react/solid';
-import Editor from '../../component/editor/Editor'
+import {
+	ArrowCircleLeftIcon,
+	ArrowCircleRightIcon
+} from "@heroicons/react/solid";
+import Editor from "../../component/editor/Editor";
+import Output from "../../component/layout/Output/Output";
 
 export async function getStaticPaths() {
-    const paths = await getAllPostIds();
-    return {
-        paths,
-        fallback: false
-    }
+	const paths = await getAllPostIds();
+	return {
+		paths,
+		fallback: false
+	};
 }
 
 export async function getStaticProps({ params }) {
-    const postData = await getPostData(params.id);
+	const postData = await getPostData(params.id);
 
-    return {
-        props: {
-            postData
-        }
-    }
+	return {
+		props: {
+			postData
+		}
+	};
 }
 export default function Javascript({ postData }) {
-    return (
-        <Layout>
-            <Head>
-                <title>{postData.title}</title>
-            </Head>
-            <div className="grid grid-cols-3 h-screen">
-                <div className="bg-gray-50 px-4 py-4">
-                    <div className="py-3">
-                        <a href="#" className=" text-sm py-1 px-2 uppercase rounded-full border border-gray-500 tracking-wider font-medium">Learn</a>
-                    </div>
-                    <div className="text-lg font-bold my-3 uppercase">
-                        <h1 className="text-lg">{postData.title}</h1>
-                    </div>
-                    <div className="text-md mt-5 mb-3">
-                        {postData.subheading}
-                    </div>
-                    <div className="text-sm font-light codeClass" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-                </div>
-                <div><Editor /></div>
-                <div></div>
-            </div>
-            <div className="flex justify-around space-x-52 h-12 bg-gray-900 items-center py-6 sticky bottom-0">
-                {postData.prev ? <Link href={`/javascript/${postData.prev}`}>
-                    <a className="text-white"><ArrowCircleLeftIcon className="h-12 w-12 inline-block text-yellow-500" />Previous</a>
-                </Link> : ''}
-                {postData.next ? <Link href={`/javascript/${postData.next}`}>
-                    <a className="text-white">Next<ArrowCircleRightIcon className="h-12 w-12 inline-block text-yellow-500" /></a>
-                </Link> : ''}
-            </div>
-        </Layout>
-    )
+	return (
+		<Layout>
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
+			<div className="grid grid-cols-3 h-screen">
+				<div className="bg-gray-50 px-4 py-4">
+					<div className="py-3">
+						<a
+							href="#"
+							className=" text-sm py-1 px-2 uppercase rounded-full border border-gray-500 tracking-wider font-medium"
+						>
+							Learn
+						</a>
+					</div>
+					<div className="text-lg font-bold my-3 uppercase">
+						<h1 className="text-lg">{postData.title}</h1>
+					</div>
+					<div className="text-md mt-5 mb-3">{postData.subheading}</div>
+					<div
+						className="text-sm font-light codeClass"
+						dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+					/>
+				</div>
+				<div>
+					<Editor />
+				</div>
+				<div>
+					<Output />
+				</div>
+			</div>
+			<div className="flex justify-around space-x-52 h-12 bg-gray-900 items-center py-6 sticky bottom-0">
+				{postData.prev ? (
+					<Link href={`/javascript/${postData.prev}`}>
+						<a className="text-white">
+							<ArrowCircleLeftIcon className="h-12 w-12 inline-block text-yellow-500" />
+							Previous
+						</a>
+					</Link>
+				) : (
+					""
+				)}
+				{postData.next ? (
+					<Link href={`/javascript/${postData.next}`}>
+						<a className="text-white">
+							Next
+							<ArrowCircleRightIcon className="h-12 w-12 inline-block text-yellow-500" />
+						</a>
+					</Link>
+				) : (
+					""
+				)}
+			</div>
+		</Layout>
+	);
 }
