1f2dabdeee01dbd23af788b7759c8bd566be74c7
akhilalekha <akhila160@gmail.com>
2021-04-28 21:23:13 +0530

    formatting mismatch - commas got removed

--- website/context/user/userReducer.js
+++ website/context/user/userReducer.js
@@ -1,18 +1,18 @@
-import { SET_PROGRESS } from "./types";
+import { SET_PROGRESS, SET_TESTS } from "./types";
 
 export default function ur(state, action) {
-  switch (action.type) {
-    case SET_PROGRESS:
-      return {
-        ...state,
-        user: action.payload,
-      };
-    case SET_TESTS:
-      return {
-        ...state,
-        test: action.payload,
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case SET_PROGRESS:
+			return {
+				...state,
+				user: action.payload
+			};
+		case SET_TESTS:
+			return {
+				...state,
+				test: action.payload
+			};
+		default:
+			return state;
+	}
 }
