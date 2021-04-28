75ef0a6deb5f91c4ac1213d0cf308eff607768f6
akhilalekha <akhila160@gmail.com>
2021-04-28 21:25:36 +0530

    fix: replaced the test case hints to match the test cases
    feat: added the setTest method which sets the state after checking the test cases
    
    fix: removed the sqaure brackets and dispatch from the value prop inside provider

--- website/context/user/UserState.js
+++ website/context/user/UserState.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from "react";
 import UserContext from "./userContext";
 import UserReducer from "./userReducer";
-import { SET_PROGRESS } from "./types";
+import { SET_TESTS, SET_PROGRESS } from "./types";
 
 const UserState = ({ children }) => {
 	const initialState = {
@@ -10,31 +10,31 @@ const UserState = ({ children }) => {
 			{
 				id: 1,
 				case: ["a=5;", "a=5"],
-				hint: "b should have value of 5",
+				hint: "a should have value of 5",
 				isCorrect: false
 			},
 			{
 				id: 2,
 				case: ["b=6;", "b=6"],
-				hint: "a should have value of 3",
+				hint: "b should have value of 6",
 				isCorrect: false
 			},
 			{
 				id: 3,
 				case: ["if(a===b){", "if(a===b)"],
-				hint: "use operator +",
+				hint: "use operator ===",
 				isCorrect: false
 			},
 			{
 				id: 4,
 				case: ["console.log('log');", "console.log('log')"],
-				hint: "use operator +",
+				hint: "use log",
 				isCorrect: false
 			},
 			{
 				id: 5,
 				case: ["}"],
-				hint: "use operator +",
+				hint: "use } ",
 				isCorrect: false
 			}
 		]
@@ -44,15 +44,21 @@ const UserState = ({ children }) => {
 
 	//set user progress - complete function
 
+	//set test
+	const setTest = testCase => {
+		dispatch({
+			type: SET_TESTS,
+			payload: testCase
+		});
+	};
+
 	return (
 		<UserContext.Provider
-			value={[
-				{
-					user: state.user,
-					test: state.test
-				},
-				dispatch
-			]}
+			value={{
+				user: state.user,
+				test: state.test,
+				setTest
+			}}
 		>
 			{children}
 		</UserContext.Provider>
