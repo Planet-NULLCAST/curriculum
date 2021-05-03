1fbf78beb865cc4ce60a9b9a7dfcead2d33f1bbc
akhilalekha <akhila160@gmail.com>
2021-05-03 18:21:24 +0530

    Fix: Add spaces to test cases
    in dataypes testcase array

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,14 +1,5 @@
-import Head from 'next/head';
-
-import Layout from '../component/layout/Layout.jsx';
-import Description from '../component/layout/common/Discription.jsx';
+import Layout from "../component/layout/Layout.jsx";
 
 export default function Home() {
-  return (
-    <Layout>
-      <div className="">
-          <Description />
-      </div>
-    </Layout>
-  )
+	return <Layout></Layout>;
 }
