1f72b313c60a4e56f9d4dc64ed46ec5b0e9ad3ea
akhilalekha <akhila160@gmail.com>
2021-05-03 18:04:29 +0530

    Refactor: Delete unused component
    Delete description

--- website/component/layout/common/Discription.jsx
+++
@@ -1,9 +0,0 @@
-export default function Description(){
-    return(
-        <div className="ml-6">
-            <div className="mb-6 mt-3">
-                <a className="py-2 px-4 bg-white text-black rounded-full text-xs border border-gray-500 border-collapse" href="#">Learn</a>
-            </div>
-        </div>
-    );
-}
