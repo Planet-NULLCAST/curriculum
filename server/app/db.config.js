3022a021f1d6de9cc5a2db52c12e875fe9e96bbd
ASEEMMUHAMMAD A. R <asaseemas@gmail.com>
2021-05-05 21:56:34 +0530

    deleted server folder

--- server/app/db.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    HOST: "localhost",
-    PORT: 27017,
-    DB: "jwtauth"
-}
--- server/app/models/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-const db = {};
-
-db.mongoose = mongoose;
-db.user = require('./user.model');
-
-module.exports = db;
