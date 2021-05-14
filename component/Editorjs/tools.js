7e9a68b95cc7bad1a6fe616a7ef5ecb5361442fa
thealpha93 <tmaakash143@gmail.com>
2021-05-14 16:05:05 +0530

    feat(): Editor JS Add editorjs and a few tools

---
+++ component/Editorjs/tools.js
@@ -0,0 +1,10 @@
+// A curated list of awesome Editor.js tools, libraries and resources. - https://github.com/editor-js/awesome-editorjs
+// All tools: https://github.com/editor-js
+
+import Header from "@editorjs/header";
+import List from "@editorjs/list"
+
+export default {
+    header: Header,
+    list: List
+}
