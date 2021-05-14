40d959f0cba126698f76e9544664ee979954e9a3
safaldas <safaldasg@gmail.com>
2021-05-14 10:29:30 +0530

    fix comma in meta file

--- courses/meta.js
+++ courses/meta.js
@@ -1,71 +1,71 @@
 import axios from "axios";
 let courses = [
-	{
-		courseId: 1,
-		courseName: "JavaScript",
-		courseUrl: "javascript",
-		chapters: [
-			{
-				chapterId: 1,
-				chapterUrl: "introduction",
-				chapterName: "Introduction"
-			},
-			{
-				chapterId: 2,
-				chapterUrl: "hello-world",
-				chapterName: "Hello World"
-			},
-			{
-				chapterId: 3,
-				chapterUrl: "variables",
-				chapterName: "Variables"
-			},
-			{
-				chapterId: 4,
-				chapterUrl: "datatypes",
-				chapterName: "Datatypes"
-			},
-			{
-				chapterId: 5,
-				chapterUrl: "logical-operators",
-				chapterName: "Logical Operators"
-			},
-			{
-				chapterId: 6,
-				chapterUrl: "if-condition",
-				chapterName: "If Condition"
-			}
-			{
-				chapterId: 7,
-				chapterUrl: "arithmetic-operators",
-				chapterName: "Arithmetic Operators"
-			}
-		]
-	},
-	{
-		courseId: 2,
-		courseName: "Python",
-		courseUrl: "python",
-		chapters: [
-			{
-				chapterId: 1,
-				chapterUrl: "introduction",
-				chapterName: "Introduction"
-			},
-			{
-				chapterId: 2,
-				chapterUrl: "hello-world",
-				chapterName: "Hello World"
-			}
-		]
-	}
+  {
+    courseId: 1,
+    courseName: "JavaScript",
+    courseUrl: "javascript",
+    chapters: [
+      {
+        chapterId: 1,
+        chapterUrl: "introduction",
+        chapterName: "Introduction",
+      },
+      {
+        chapterId: 2,
+        chapterUrl: "hello-world",
+        chapterName: "Hello World",
+      },
+      {
+        chapterId: 3,
+        chapterUrl: "variables",
+        chapterName: "Variables",
+      },
+      {
+        chapterId: 4,
+        chapterUrl: "datatypes",
+        chapterName: "Datatypes",
+      },
+      {
+        chapterId: 5,
+        chapterUrl: "logical-operators",
+        chapterName: "Logical Operators",
+      },
+      {
+        chapterId: 6,
+        chapterUrl: "if-condition",
+        chapterName: "If Condition",
+      },
+      {
+        chapterId: 7,
+        chapterUrl: "arithmetic-operators",
+        chapterName: "Arithmetic Operators",
+      },
+    ],
+  },
+  {
+    courseId: 2,
+    courseName: "Python",
+    courseUrl: "python",
+    chapters: [
+      {
+        chapterId: 1,
+        chapterUrl: "introduction",
+        chapterName: "Introduction",
+      },
+      {
+        chapterId: 2,
+        chapterUrl: "hello-world",
+        chapterName: "Hello World",
+      },
+    ],
+  },
 ];
 
 async function addCourses() {
-	// console.log("in add courses");
-	axios
-		.post("http://localhost:8080", courses)
-		.then((res) => console.log("sent courses", res.data))
-		.catch((err) => console.log(err));
+  // console.log("in add courses");
+  axios
+    .post("http://localhost:8080", courses)
+    .then((res) => console.log("sent courses", res.data))
+    .catch((err) => console.log(err));
 }
 export { courses, addCourses };
