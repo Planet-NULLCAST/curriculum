9ad71ff1a1f00920607145375bcddfb632414f91
Sangeeth S <sangeeth.s@neoito.com>
2021-05-17 15:15:13 +0530

    profile ui changes

--- component/profile/Navbar.jsx
+++ component/profile/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 export default function Navbar() {
-	return (
-		<div className="bg-white h-12 my-7 flex flex-row items-center text-gray-500 rounded shadow-sm">
-			<ul className="flex flex-row justify-start items-center font-semibold">
-				<li className="m-4">Profile</li>
-				<li className="m-4">My Store</li>
-				<li className="m-4">Achievements</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div className="bg-white h-12 my-3 flex flex-row items-center rounded shadow-sm">
+      <ul className="flex flex-row justify-start items-center font-semibold h-full">
+        <li className="mx-4 cursor-pointer border-b-4 border-gray-600 h-full flex justify-center items-center">
+          <span className="mt-1 text-gray-600">Profile</span>
+        </li>
+        <li className="mx-4 cursor-pointer border-b-4 border-white h-full flex justify-center items-center">
+          <span className="mt-1 text-gray-400">My Store</span>
+        </li>
+        <li className="mx-4 cursor-pointer border-b-4 border-white h-full flex justify-center items-center">
+          <span className="mt-1 text-gray-400">Achievements</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- pages/u/[username].js
+++ pages/u/[username].js
@@ -5,23 +5,25 @@ import Head from "next/head";
 import Activity from "../../component/profile/Activity";
 
 export default function Username() {
-	return (
-		<div className="bg-gray-200 py-4 px-16">
-			<Head>
-				<title>Profile | Nullcast</title>
-			</Head>
-			<Navbar />
-			<div className="flex flex-row">
-				<div className="flex flex-col w-3/4">
-					<ProfileDetails />
-					<Activity />
-					{/* <div className="bg-white shadow-sm rounded p-4 my-4 h-80">dd</div> */}
-					<div className="bg-white shadow-sm rounded p-4 mb-4 h-80">dd</div>
-				</div>
-				<div className="bg-white shadow-sm rounded h-auto w-1/4 ml-4 mb-4">
-					side
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <>
+      <Head>
+        <title>Profile | Nullcast</title>
+      </Head>
+      <div className="bg-gray-200 py-2 px-16">
+        <Navbar />
+        <div className="flex flex-row">
+          <div className="flex flex-col w-3/4">
+            <ProfileDetails />
+            <Activity />
+            {/* <div className="bg-white shadow-sm rounded p-4 my-4 h-80">dd</div> */}
+            <div className="bg-white shadow-sm rounded p-4 mb-4 h-80">dd</div>
+          </div>
+          <div className="bg-white shadow-sm rounded h-auto w-1/4 ml-4 mb-4">
+            side
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
