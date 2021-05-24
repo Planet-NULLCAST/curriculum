0d56602e6c24049045ffec8b66fa8eb3a28ba677
akhilalekha <akhila160@gmail.com>
2021-05-24 20:17:08 +0530

    remove comments

--- config/config.js
+++ config/config.js
@@ -4,6 +4,3 @@ const authUrl = "/api/auth";
 const enrolUrl = "/api/enrol";
 
 export { baseUrl, clientUrl, authUrl, enrolUrl };
-
-// BASE_URL = "test-api.nullcast.io";
-// CLIENT_URL = "v2.nullcast.io";
