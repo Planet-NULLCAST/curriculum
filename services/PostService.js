8d38c4c620655237aa6a98278fbe72655fd6ad0a
akhilalekha <akhila160@gmail.com>
2021-06-14 13:27:57 +0530

    Feat: add function for changing status - change in getposts - replace get with post

--- services/PostService.js
+++ services/PostService.js
@@ -1,10 +1,15 @@
 const axios = require("axios");
-import { baseUrl, allPostsUrl, postUrl, s3Url } from "../config/config";
-import env from "../next.config";
+import {
+  baseUrl,
+  allPostsUrl,
+  postUrl,
+  s3Url,
+  changeStatusUrl
+} from "../config/config";
 
-async function getPostsByUserId(userCookie) {
+async function getPostsByUserId(userCookie, reqData) {
   try {
-    const { data } = await axios.get(`${baseUrl}/${allPostsUrl}`, {
+    const { data } = await axios.post(`${baseUrl}/${allPostsUrl}`, reqData, {
       headers: {
         "x-access-token": `${userCookie.accessToken}`
       }
@@ -104,6 +109,26 @@ async function uploadImage(imageFile, imageData) {
     return;
   }
 }
+
+async function changePostStatus(userCookie, postId, statusUpdate) {
+  try {
+    const { data } = await axios.post(
+      `${baseUrl}/${changeStatusUrl}/${postId}`,
+      statusUpdate,
+      {
+        headers: {
+          "x-access-token": `${userCookie.accessToken}`
+        }
+      }
+    );
+    // console.log(data.message);
+    return data.message;
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+}
+
 const PostService = {
   getPostById,
   getPostsByUserId,
@@ -111,7 +136,8 @@ const PostService = {
   createPost,
   updatePostById,
   deletePostById,
-  uploadImage
+  uploadImage,
+  changePostStatus
 };
 
 module.exports = PostService;
