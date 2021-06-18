33c42b66fc92e0244115fe796411cb8194310a66
athulraj2002 <athulraj2002@gmail.com>
2021-06-18 11:27:30 +0530

    env updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  productionBrowserSourceMaps: process.env.GENERATE_SOURCEMAP,
   images: {
     domains: ['nullcast-assets.s3.amazonaws.com'],
   },
@@ -13,7 +14,8 @@ module.exports = {
     ENV: process.env.ENV,
     EDITOR_URL: process.env.EDITOR_URL,
     BUCKET_URL: process.env.BUCKET_URL,
-    SERVER_URL : process.env.SERVER_URL
+    SERVER_URL : process.env.SERVER_URL,
+
   },
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")]
