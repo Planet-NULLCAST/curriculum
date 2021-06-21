81be0f3f52c655782ae9e665230ce7b873067795
akhilalekha <akhila160@gmail.com>
2021-06-21 14:12:33 +0530

    Fix: add tag page link - add updatedAt instead of publishedAt temporarily

--- component/layout/BlogListing/ListingFeatured.jsx
+++ component/layout/BlogListing/ListingFeatured.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import Fade from "react-reveal/Fade";
 import Details from "./AuthorDetails";
-import moment from 'moment'
+import moment from "moment";
 
 export default function ListingFeatured(props) {
   return (
@@ -25,26 +25,24 @@ export default function ListingFeatured(props) {
         <Fade>
           <div className={` ${styles.text} md:w-2/3 md:pl-6`}>
             <div className="tags">
-              <a className="tags__item">
-                {props.blog[0]?.tags[0]}
-              </a>
+              <Link href={`/tag/${props.blog[0]?.tags[0]}`}>
+                <a className="tags__item">{props.blog[0]?.tags[0]}</a>
+              </Link>
             </div>
             <h3>
               <Link href={`/${props.blog[0].slug}`}>
                 <a>{props.blog[0]?.title}</a>
               </Link>
             </h3>
-            <p>
-              {props.blog[0]?.metaDescription}
-            </p>
+            <p>{props.blog[0]?.metaDescription}</p>
             <p className={styles.time}>
-                <span>{moment(props.blog[0]?.publishedAt).format('MMMM Do, YYYY')}</span>
-                {/* <span>5 Min Read</span> */}
+              <span>
+                {moment(props.blog[0]?.updatedAt).format("MMMM Do, YYYY")}
+              </span>
+              {/* <span>5 Min Read</span> */}
             </p>
 
-            <Details 
-              username={props.blog[0]?.primaryAuthor.username}
-            />
+            <Details username={props.blog[0]?.primaryAuthor.username} />
           </div>
         </Fade>
       </div>
