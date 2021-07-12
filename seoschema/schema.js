77819c7611bf9ececd4dc947c3f493441cb151d8
akhilalekha <akhila160@gmail.com>
2021-07-12 15:41:15 +0530

    Feat: Add meta tags, schema

---
+++ seoschema/schema.js
@@ -0,0 +1,15 @@
+export const url = "https://nullcast.io";
+export const logoPath = "images/nullcast.png";
+export const homePageSchema = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Nullcast",
+  "url": `${url}`,
+  "logo": `${logoPath}`,
+  "sameAs": [
+    "https://www.facebook.com/nullcast",
+    "https://twitter.com/nullcast_io",
+    ""
+  ],
+  "description": "Thoughts, stories and ideas."
+};
