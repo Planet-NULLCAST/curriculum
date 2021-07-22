671e66b69f38b0aa1cdc88a996c59a5d77c6165a
safaldas <safaldasg@gmail.com>
2021-07-22 22:55:31 +0530

    fix fallback dummy image in section users for v2data

--- component/layout/SectionUsers/SectionUsers.jsx
+++ component/layout/SectionUsers/SectionUsers.jsx
@@ -26,9 +26,9 @@ export default class SimpleSlider extends Component {
                 <div key={key}>
                   <div className={styles.item}>
                     <div className={styles.icon}>
-                      <Image
+                    <Image
                         className="rounded-full"
-                        src={item.avatar}
+                        src={item.avatar||`/images/dummy${key%4}.png`}
                         alt="user"
                         height="70rem"
                         width="70rem"
