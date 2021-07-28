234dfed0570280d866335876c8a686cfce3b781e
akhilalekha <akhila160@gmail.com>
2021-07-28 13:38:43 +0530

    Fix: set limit to 10

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import UserState from "../context/user/UserState";
 import "react-toastify/dist/ReactToastify.css";
-import "../styles/globals.scss";
 import "../styles/slick.scss";
 import "../styles/slick-theme.scss";
+import "../styles/globals.scss";
 import "../styles/Home.module.scss";
 import { ToastContainer } from "react-toastify";
 
