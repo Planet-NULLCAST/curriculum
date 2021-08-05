b6fa2d5b55857a0e0c23b4b7ac614bb25d2bb56d
akhilalekha <akhila160@gmail.com>
2021-08-05 19:59:06 +0530

    Fix: change content

--- component/layout/ChapterOverview/ChapterOverview.jsx
+++ component/layout/ChapterOverview/ChapterOverview.jsx
@@ -10,18 +10,17 @@ export default function ChapterOverview() {
         <div className="row md:flex">
           <div className="md:w-8/12 pt-5 md:text-18 font-medium lg:font-semibold leading-loose mb-10 md:mb-0 md:pr-5">
             <p className="mb-10">
-              Without CSS, every web page would be drab plain text and images
-              that flowed straight down the page. With CSS, you can add color
-              and background images and change the layout of your page — your
-              web pages can feel like works of art!
+              JavaScript (JS) is a lightweight, interpreted, or just-in-time
+              compiled programming language with first-class functions. While it
+              is most well-known as the scripting language for Web pages, many
+              non-browser environments also use it, such as Node.js, Apache
+              CouchDB etc.
             </p>
             <h3 className="text-25 font-extrabold mb-5">Take-Away Skills:</h3>
             <p>
-              You will learn many aspects of styling web pages! You’ll be able
-              to set up the correct file structure, edit text and colors, and
-              create attractive layouts. With these skills, you’ll be able to
-              customize the appearance of your web pages to suit your every
-              need!
+              You will learn important basics of JavaScript. We will cover
+              JavaScript's key fundamental features, turning our attention to
+              it's key features such as variables, datatypes, functions.
             </p>
           </div>
           <div className="md:w-4/12">
@@ -30,13 +29,13 @@ export default function ChapterOverview() {
                 <h3>Certificate</h3>
                 <p>of completion for free</p>
               </div>
-              <div className={styles.item}>
+              {/* <div className={styles.item}>
                 <h3>490,362</h3>
                 <p>People who have taken this course</p>
-              </div>
+              </div> */}
               <div className={styles.item}>
                 <p>Time to Complete</p>
-                <h3>10 Hours</h3>
+                <h3>~ 4 Hours</h3>
               </div>
             </div>
           </div>
