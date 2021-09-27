60b121bcc0912ed5d3f6f2cc75f7ba808215b17d
jasirtp <jasir.tp13@gmail.com>
2021-09-27 22:22:45 +0530

    fix: api call relocation
    try catch added in login signup chapteName

--- pages/login.js
+++ pages/login.js
@@ -9,7 +9,7 @@ import Link from "next/link";
 import Fade from "react-reveal/Fade";
 import Cookies from "universal-cookie";
 import moment from "moment";
-import {signIn} from "../services/AuthService"
+import { signIn } from "../services/AuthService";
 
 import { getCookieValue } from "../lib/cookie";
 
@@ -17,10 +17,7 @@ export async function getServerSideProps(context) {
   // console.log(context.req.headers.referer);
   try {
     if (context.req.headers.cookie) {
-      const contextCookie = getCookieValue(
-        context.req.headers.cookie,
-        "token"
-      );
+      const contextCookie = getCookieValue(context.req.headers.cookie, "token");
       if (contextCookie) {
         return {
           redirect: {
@@ -82,88 +79,93 @@ export default function Login({ referer }) {
   const notify = (err) =>
     toast.error(err.message, {
       position: "top-right",
-      autoClose: 5000,
+      autoClose: 3000,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
       draggable: true,
       progress: undefined
     });
-  async function handleClick (e) {
+  async function handleClick(e) {
     e.preventDefault();
     setIsLoading(true);
     // console.log(e.target);
     if (validEmail && validPassword) {
       const password = document.querySelector("#password").value;
       const email = document.querySelector("#email").value;
-      
+
       if (password && email) {
-        const data = await signIn(email, password)
-            const cookies = new Cookies();
-            const userToken = cookies.get("token");
-						const newDate = new Date(moment().add(30, 'days')).toUTCString();
-						const expires = `; expires=${newDate}`;
-						const userData = data.user;
-
-            if (userToken) {
-							document.cookie = `userNullcast=${JSON.stringify(userData)}${expires}`;
-              localStorage.setItem("userNullcast", JSON.stringify(userData));
-							// console.log(document.cookie);
-
-              // let progress = JSON.parse(
-              //   window.localStorage.getItem("progress")
-              // ) || [{ courseName: "", completedChapter: [] }];
-              // axios({
-              //   method: "post",
-              //   url: `${baseUrl}${enrolUrl}/progress`,
-              //   headers: {
-              //     "x-access-token": `${data.accessToken}`
-              //   },
-              //   data: progress
-              // }).then((response) => {
-              //   // console.log(response);
-              // });
-              // axios({
-              //   method: "post",
-              //   url: `${baseUrl}/api/progress/all`,
-              //   headers: {
-              //     "x-access-token": `${data.accessToken}`
-              //   }
-              // })
-              //   .then((response) => {
-              //     window.localStorage.setItem(
-              //       "progress",
-              //       JSON.stringify(response.data)
-              //     );
-              //   })
-              //   .catch((err) => {
-              //     console.log(err.message);
-              //   });
-              // if (referer) {
-              //   router.back();
-              // } else {
-              //   router.push("/");
-              // }
-              if (referer) {
-                router.back();
-              } else {
-                router.push("/");
-              }
+        try {
+          const data = await signIn(email, password);
+          const cookies = new Cookies();
+          const userToken = cookies.get("token");
+          const newDate = new Date(moment().add(30, "days")).toUTCString();
+          const expires = `; expires=${newDate}`;
+          const userData = data.user;
+          if (userToken) {
+            document.cookie = `userNullcast=${JSON.stringify(
+              userData
+            )}${expires}`;
+            localStorage.setItem("userNullcast", JSON.stringify(userData));
+            // console.log(document.cookie);
+  
+            // let progress = JSON.parse(
+            //   window.localStorage.getItem("progress")
+            // ) || [{ courseName: "", completedChapter: [] }];
+            // axios({
+            //   method: "post",
+            //   url: `${baseUrl}${enrolUrl}/progress`,
+            //   headers: {
+            //     "x-access-token": `${data.accessToken}`
+            //   },
+            //   data: progress
+            // }).then((response) => {
+            //   // console.log(response);
+            // });
+            // axios({
+            //   method: "post",
+            //   url: `${baseUrl}/api/progress/all`,
+            //   headers: {
+            //     "x-access-token": `${data.accessToken}`
+            //   }
+            // })
+            //   .then((response) => {
+            //     window.localStorage.setItem(
+            //       "progress",
+            //       JSON.stringify(response.data)
+            //     );
+            //   })
+            //   .catch((err) => {
+            //     console.log(err.message);
+            //   });
+            // if (referer) {
+            //   router.back();
+            // } else {
+            //   router.push("/");
+            // }
+            if (referer) {
+              router.back();
             } else {
-              setIsLoading(false);
-              notify(data);
+              router.push("/");
             }
-          };
-      } else {
-        setIsLoading(false);
-        if (!email) {
-          setEmailValid(false);
-        }
-        if (!password) {
-          setValidPassword(false);
+          } else {
+            setIsLoading(false);
+            notify(data);
+          }
+        } catch (err) {
+          setIsLoading(false);
+          notify(err);
         }
       }
-  };
+    } else {
+      if (!email) {
+        setEmailValid(false);
+      }
+      if (!password) {
+        setValidPassword(false);
+      }
+    }
+  }
   return (
     <div>
       <Head>
