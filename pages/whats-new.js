4e7ddd081f08268b2fd3167ac980f63af2cbcecd
Pavittar Singh <29155477+pavittarx@users.noreply.github.com>
2021-10-28 04:14:55 +0000

    fix: props.blogs changed to blogs

--- pages/whats-new.js
+++ pages/whats-new.js
@@ -45,7 +45,7 @@ export default function whatsNew({blogs}) {
       </Head>
       <SiteHeader />
       <WhatsNewSpotlight />
-      { blogs?.length > 0 && <WhatsNewPosts blogs={props.blogs} />}
+      { blogs?.length > 0 && <WhatsNewPosts blogs={blogs} />}
       <SectionSwag />
       <SiteFooter />
     </>
