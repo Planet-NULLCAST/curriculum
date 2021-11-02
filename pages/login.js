94ec4bba1e7874a960d6655c7bc465b9648abdc6
Mrinmay Santra <mrinmay7875@gmail.com>
2021-11-03 02:49:56 +0530

    style: redesigned login page
    redesigned login page and changed its look
    
    fixes issue #360

--- pages/login.js
+++ pages/login.js
@@ -7,7 +7,7 @@ import { LoadIcon } from "../component/ButtonLoader/LoadIcon";
 import Head from "next/head";
 import Link from "next/link";
 import Fade from "react-reveal/Fade";
-import Cookies from "universal-cookie";
+// import Cookies from "universal-cookie";
 import moment from "moment";
 import { signIn } from "../services/AuthService";
 
@@ -18,7 +18,7 @@ export async function getServerSideProps(context) {
   // console.log(context.req.headers.referer);
   try {
     if (context.req.headers.cookie) {
-      const contextCookie = getCookieValue(context.req.headers.cookie, "userNullcast");
+      const contextCookie = getCookieValue(context.req.headers.cookie, "token");
       if (contextCookie) {
         return {
           redirect: {
@@ -87,7 +87,10 @@ export default function Login({ referer }) {
       return;
     }
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if(document.querySelector("#email").value && !document.querySelector("#email").value.match(regexEmail)){
+    if (
+      document.querySelector("#email").value &&
+      !document.querySelector("#email").value.match(regexEmail)
+    ) {
       setEmailValid(false);
       return;
     }
@@ -151,7 +154,7 @@ export default function Login({ referer }) {
           }
         } catch (err) {
           setIsLoading(false);
-          notify(err?.response?.data?.message ?? err?.message, 'error');
+          notify(err?.response?.data?.message ?? err?.message, "error");
         }
       }
     } else {
@@ -176,142 +179,136 @@ export default function Login({ referer }) {
         ></img>
       </Link>
       <div
-        className={`w-full h-screen flex ${Loginstyles.bg_yellow_710} loginSection`}
+        className={`p-20 h-screen w-screen flex flex-col-reverse md:flex-row items-center justify-center w-full  ${Loginstyles.bg_yellow_710} loginSection`}
       >
         <SideLogin />
-        <div className="flex justify-end w-full items-center">
+        <Fade duration={2000}>
           <div
-            className={`${Loginstyles.bg_yellow_710} h-full flex flex-col items-center lg:items-end justify-center overflow-x-hidden greenLoginDiv`}
+            className={`${Loginstyles.bg_yellow_710} h-full  lg:mr-30 flex flex-col items-center  justify-center  greenLoginDiv  `}
           >
-            <div
-              className={`rounded-xl w-full py-5 sm:py-10 px-5 sm:px-10 md:px-5 lg:px-20 max-w-xl flex items-start justify-center flex-col greenLogin relative overflow-y-auto ${Loginstyles.formCard}`}
+            <form
+              className={`shadow-lg w-80 p-4 z-10 lg:ml-30 lg:mr-10  flex flex-col bg-black rounded-lg {Loginstyles.bg_yellow_710}`}
             >
-              <Fade duration={2000}>
-                <div
-                  className={`absolute top-0 right-0 flex items-center justify-end p-6 w-full ${Loginstyles.bg_green_710}`}
-                >
-                  <p
-                    className={`sm:flex hidden font-semibold text-white text-sm mr-2 md:mr-5`}
-                  >
-                    Don’t have an Account ?
-                  </p>
-                  {isLoading ? (
-                    <div className="md:mr-4 bg-pink-710 font-semibold border border-pink-710 rounded-sm duration-700 text-white focus:outline-none flex justify-center items-center w-20 h-10 opacity-50 cursor-not-allowed">
-                      Sign Up
-                    </div>
-                  ) : (
-                    <Link
-                      href={{
-                        pathname: `/signup`
-                      }}
-                    >
-                      <div className="md:mr-4 bg-pink-710 font-semibold hover:bg-transparent hover-text-pink-710 border border-pink-710 rounded-sm duration-700 text-white focus:outline-none cursor-pointer flex justify-center items-center w-20 h-10">
-                        Sign Up
-                      </div>
-                    </Link>
-                  )}
-                </div>
-                <h1 className="text-white font-bold text-2xl leading-10">
-                  Login
-                </h1>
-                <p className={`text-white mt-2 text-sm font-bold mb-3`}>
-                  Welcome Back!
-                </p>
-                <div className="container py-2 px-0-imp">
-                  <form method="" action="">
-                    <div className="mb-1 flex flex-col">
-                      <label
-                        className={`text-white mt-2 mb-1 font-semibold text-sm`}
-                      >
-                        Email
-                      </label>
-                      <input
-                        placeholder="Enter email"
-                        maxLength="30"
-                        className={`inputStyle placeholder-gray-600 pr-3 ${Loginstyles.inputGreen}`}
-                        id="email"
-                        name="email"
-                        type="text"
-                        //onBlur={(e) => emailValidator(e)}
-                        onChange={(e) => {
-                          setEmailValid(true)
-                          // if (!validEmail) {
-                          //   emailValidator(e);
-                          // }
-                        }}
-                      />
-                      {validEmail ? (
-                        ""
-                      ) : (
-                        <span className="flex items-center font-bold tracking-wide text-red-danger text-xs mt-1 ml-0">
-                          Invalid email field !
-                        </span>
-                      )}
-                    </div>
-                    <div className="mb-4 flex flex-col">
-                      <label
-                        className={`text-white mt-2 mb-1 font-semibold text-sm`}
-                      >
-                        Password
-                      </label>
-                      <div className="relative w-full">
-                        <input
-                          placeholder="Enter password"
-                          maxLength="50"
-                          className={`inputStyle placeholder-gray-600 w-full pr-10 ${Loginstyles.inputGreen}`}
-                          id="password"
-                          name="password"
-                          onBlur={(e) => handlePassword(e)}
-                          onChange={(e) => {
-                            if (!validPassword) {
-                              handlePassword(e);
-                            }
-                          }}
-                          type={`${hidePassword ? "text" : "password"}`}
-                        />
-                        <div className="flex justify-center items-center items h-full absolute right-0 top-0 w-10">
-                          <img
-                            src="/images/eye.svg"
-                            className="w-1/2 cursor-pointer opacity-50 hover:opacity-100 duration-700"
-                            onClick={(e) => eyeClick(e)}
-                          ></img>
-                        </div>
-                      </div>
-                      {validPassword ? (
-                        ""
-                      ) : (
-                        <span className="flex items-center font-bold tracking-wide text-red-danger text-xs mt-1 ml-0">
-                          Please enter password
-                        </span>
-                      )}
-                    </div>
+              <h1 className="lg:hidden xl:block mb-1 text-white font-bold text-2xl leading-10">
+                Login
+              </h1>
+              <p
+                className={`lg:hidden xl:block text-white mt-0 font-bold text-xs  mb-3`}
+              >
+                Welcome Back!
+              </p>
+              <div className="mb-1 flex flex-col">
+                <label className={`text-white mt-2 mb-1 font-semibold text-sm`}>
+                  Email
+                </label>
+                <input
+                  placeholder="Enter email"
+                  maxLength="30"
+                  className={`inputStyle placeholder-gray-600 pr-3 ${Loginstyles.inputGreen} xl:mb-2.5 2xl:mb-5 `}
+                  id="email"
+                  name="email"
+                  type="email"
+                  onBlur={(e) => emailValidator(e)}
+                  onChange={(e) => {
+                    emailValidator(e);
 
-                    <button
-                      className={`submitButtons w-full flex items-center justify-center ${
-                        isLoading
-                          ? "opacity-50 cursor-not-allowed"
-                          : "hover:bg-transparent hover-text-pink-710"
-                      }`}
-                      type="submit"
-                      onClick={(e) => {
-                        handleClick(e);
-                      }}
-                      disabled={!validEmail || !validPassword || isLoading}
-                    >
-                      {isLoading && <LoadIcon color="#fff" height="23px" />}
-                      Login
-                    </button>
-                    <Link href="/forgot-password">
-                      <a className="text-white text-sm my-5">
-                        Forgot Password ?
-                      </a>
-                    </Link>
-                  </form>
+                    // setEmailValid(true);
+                    // if (!validEmail) {
+                    //   emailValidator(e);
+                    // }
+                  }}
+                />
+                {validEmail ? (
+                  ""
+                ) : (
+                  <span className="flex items-center font-bold tracking-wide text-red-danger text-xs pt-1 ml-0">
+                    Invalid email field !
+                  </span>
+                )}
+              </div>
+              <div className="mb-4 flex flex-col">
+                <label className={`text-white mt-2 mb-1 font-semibold text-sm`}>
+                  Password
+                </label>
+                <div className="relative w-full">
+                  <input
+                    placeholder="Enter password"
+                    maxLength="50"
+                    className={`inputStyle placeholder-gray-600 w-full pr-10 ${Loginstyles.inputGreen} xl:mb-3 2xl:mb-5`}
+                    id="password"
+                    name="password"
+                    onBlur={(e) => handlePassword(e)}
+                    onChange={(e) => {
+                      if (!validPassword) {
+                        handlePassword(e);
+                      }
+                    }}
+                    type={`${hidePassword ? "text" : "password"}`}
+                  />
+                  <div className="flex justify-center items-center items h-full absolute right-0 top-0 w-10">
+                    <img
+                      alt="eye"
+                      src="/images/eye.svg"
+                      className="w-1/2 cursor-pointer xl:mb-3 opacity-50 hover:opacity-100 duration-700"
+                      onClick={(e) => eyeClick(e)}
+                    ></img>
+                  </div>
                 </div>
-              </Fade>
-            </div>
+                {validPassword ? (
+                  ""
+                ) : (
+                  <span className="flex items-center font-bold tracking-wide text-red-danger text-xs pt-1 ml-0">
+                    Please enter password
+                  </span>
+                )}
+              </div>
+
+              <button className="w-full bg-blue-500 text-white p-1.5 2xl:p-3 rounded-lg font-semibold text-lg">
+                Login
+              </button>
+              <Link href="/forgot-password">
+                <a className="text-blue-400 text-center my-2">
+                  Forgot Pasword?
+                </a>
+              </Link>
+              <hr />
+
+              {/* <div
+              className={`absolute top-0 right-0 flex items-center justify-end p-6 w-full ${Loginstyles.bg_green_710}`}
+            >
+              {isLoading ? (
+                <div className="md:mr-4 bg-pink-710 font-semibold border border-pink-710 rounded-sm duration-700 text-white focus:outline-none flex justify-center items-center w-20 h-10 opacity-50 cursor-not-allowed">
+                  Sign Up
+                </div>
+              ) : (
+                <Link
+                  href={{
+                    pathname: `/signup`
+                  }}
+                >
+                  <div className="md:mr-4 bg-pink-710 font-semibold hover:bg-transparent hover-text-pink-710 border border-pink-710 rounded-sm duration-700 text-white focus:outline-none cursor-pointer flex justify-center items-center w-20 h-10">
+                    Sign Up
+                  </div>
+                </Link>
+              )}
+
+           </div> */}
+
+              <p className={`w-full text-center text-white pt-2 `}>
+                Don’t have an Account ?
+              </p>
+              <Link
+                href={{
+                  pathname: `/signup`
+                }}
+              >
+                <button className="w-full bg-green-400 mt-5 2xl:mt-8 xl:mb-2.5 text-white p-1.5 2xl:p-3 rounded-lg font-semibold text-lg">
+                  Sign Up
+                </button>
+              </Link>
+            </form>
           </div>
-        </div>
+        </Fade>
       </div>
     </div>
   );
