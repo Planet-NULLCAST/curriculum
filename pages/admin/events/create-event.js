c030304f57a565e68cd62924493d816f0c9600fc
jasirneoito <jasir@neoito.com>
2021-12-01 15:36:35 +0530

    fix: event update issue fixed #618
    fixed event update page issues

--- pages/admin/events/create-event.js
+++ pages/admin/events/create-event.js
@@ -12,8 +12,6 @@ import { useRouter } from "next/router";
 import SharedService from "../../../services/SharedService";
 import { getCookieValue } from "../../../lib/cookie";
 
-
-
 export async function getServerSideProps(context) {
   try {
     if (context.req.headers.cookie) {
@@ -77,89 +75,107 @@ export async function getServerSideProps(context) {
   }
 }
 
-export async function getImageUrl(eventData , response) {
-  return Promise.all([SharedService.uploadImage(eventData.guest_image , {
-    stage: "dev",
-    fileName: eventData.guest_image.name,
-    id: response.data.data.id,
-    category: "events",
-    ContentType: "image/png"
-  }), SharedService.uploadImage(eventData.banner_image , {
-    stage: "dev",
-    fileName: eventData.banner_image.name,
-    id: response.data.data.id,
-    category: "events",
-    ContentType: "image/png"
-  })])
+export async function getImageUrl(eventData, response) {
+  return Promise.all([
+    SharedService.uploadImage(eventData.guest_image, {
+      stage: "dev",
+      fileName: eventData.guest_image.name,
+      id: response.data.data.id,
+      category: "events",
+      ContentType: "image/png"
+    }),
+    SharedService.uploadImage(eventData.banner_image, {
+      stage: "dev",
+      fileName: eventData.banner_image.name,
+      id: response.data.data.id,
+      category: "events",
+      ContentType: "image/png"
+    })
+  ]);
 }
 
+const validateForm = (eventDetails, setEventDetailsError) => {
+  let isValid = false;
+  if (eventDetails.organizerName.trim()) {
+    setEventDetailsError((prev) => ({ ...prev, organizerNameError: "" }));
+  } else {
+    isValid = false;
+    setEventDetailsError((prev) => ({
+      ...prev,
+      organizerNameError: "Enter Organizer's Name"
+    }));
+  }
+  if (eventDetails.tagLine.trim()) {
+    setEventDetailsError((prev) => ({ ...prev, tagLineError: "" }));
+  } else {
+    isValid = false;
+    setEventDetailsError((prev) => ({
+      ...prev,
+      tagLineError: "Enter Organizer's Tag"
+    }));
+  }
+  if (eventDetails.eventName.trim()) {
+    setEventDetailsError((prev) => ({ ...prev, eventNameError: "" }));
+  } else {
+    isValid = false;
+    setEventDetailsError((prev) => ({
+      ...prev,
+      eventNameError: "Enter Event Name"
+    }));
+  }
+  if (eventDetails.eventLocation.trim()) {
+    setEventDetailsError((prev) => ({ ...prev, eventLocationError: "" }));
+  } else {
+    isValid = false;
+    setEventDetailsError((prev) => ({
+      ...prev,
+      eventLocationError: "Enter Event Location"
+    }));
+  }
+  if (eventDetails.eventDescription.trim()) {
+    setEventDetailsError((prev) => ({ ...prev, eventDescriptionError: "" }));
+  } else {
+    isValid = false;
+    setEventDetailsError((prev) => ({
+      ...prev,
+      eventDescriptionError: "Enter Event Description"
+    }));
+  }
+  if (eventDetails.eventLink.trim()) {
+    setEventDetailsError((prev) => ({ ...prev, eventLinkError: "" }));
+  } else {
+    isValid = false;
+    setEventDetailsError((prev) => ({
+      ...prev,
+      eventLinkError: "Enter Event Link"
+    }));
+  }
+  if (eventDetails.eventDate.trim()) {
+    setEventDetailsError((prev) => ({ ...prev, eventDateError: "" }));
+  } else {
+    isValid = false;
+    setEventDetailsError((prev) => ({
+      ...prev,
+      eventDateError: "Enter Event Date"
+    }));
+  }
+  if (eventDetails.eventTime.trim()) {
+    setEventDetailsError((prev) => ({ ...prev, eventTimeError: "" }));
+  } else {
+    isValid = false;
+    setEventDetailsError((prev) => ({
+      ...prev,
+      eventTimeError: "Enter Event Time"
+    }));
+  }
 
-const validateForm = (eventDetails , setEventDetailsError) => {
-  let isValid = false
-  if(eventDetails.organizerName.trim()){
-    setEventDetailsError(prev => ({...prev , organizerNameError : ""}))
-  }
-  else {
-    isValid = false
-    setEventDetailsError(prev => ({...prev , organizerNameError : "Enter Organizer's Name"}))
-  }
-  if(eventDetails.tagLine.trim()){
-    setEventDetailsError(prev => ({...prev , tagLineError : ""}))
-  }
-  else {
-    isValid = false
-    setEventDetailsError(prev => ({...prev , tagLineError : "Enter Organizer's Tag"}))
-  }
-  if(eventDetails.eventName.trim()){
-    setEventDetailsError(prev => ({...prev , eventNameError : ""}))
-  }
-  else {
-    isValid = false
-    setEventDetailsError(prev => ({...prev , eventNameError : "Enter Event Name"}))
-  }
-  if(eventDetails.eventLocation.trim()){
-    setEventDetailsError(prev => ({...prev , eventLocationError : ""}))
-  }
-  else {
-    isValid = false
-    setEventDetailsError(prev => ({...prev , eventLocationError : "Enter Event Location"}))
-  }
-  if(eventDetails.eventDescription.trim()){
-    setEventDetailsError(prev => ({...prev , eventDescriptionError : ""}))
-  }
-  else {
-    isValid = false
-    setEventDetailsError(prev => ({...prev , eventDescriptionError : "Enter Event Description"}))
-  }
-  if(eventDetails.eventLink.trim()){
-    setEventDetailsError(prev => ({...prev , eventLinkError : ""}))
-  }
-  else {
-    isValid = false
-    setEventDetailsError(prev => ({...prev , eventLinkError : "Enter Event Link"}))
-  }
-  if(eventDetails.eventDate.trim()){
-    setEventDetailsError(prev => ({...prev , eventDateError : ""}))
-  }
-  else {
-    isValid = false
-    setEventDetailsError(prev => ({...prev , eventDateError : "Enter Event Date"}))
-  }
-  if(eventDetails.eventTime.trim()){
-    setEventDetailsError(prev => ({...prev , eventTimeError : ""}))
-  }
-  else {
-    isValid = false
-    setEventDetailsError(prev => ({...prev , eventTimeError : "Enter Event Time"}))
-  }
-
-  return isValid
-}
+  return isValid;
+};
 
 const CreateEvent = () => {
-  const router = useRouter()
+  const router = useRouter();
   const [eventID, setEventID] = useState(router.query.id);
-  const [isLoading , setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     setTimeout(() => {
       eventID && getEvents(eventID);
@@ -169,14 +185,14 @@ const CreateEvent = () => {
     try {
       const data = await EventService.getEventById(reqData);
       const finalData = data.data;
-      const date = moment(finalData.event_time).format('YYYY-MM-DD')
-      const time = moment(finalData.event_time).format('HH:SS')
+      const date = moment(finalData.event_time).format("YYYY-MM-DD");
+      const time = moment(finalData.event_time).format("HH:SS");
       setEventDetails({
         organizerImage: finalData.guest_image,
         organizerName: finalData.guest_name,
         tagLine: finalData.guest_designation,
-        eventName:finalData.title,
-        eventLocation:finalData.location,
+        eventName: finalData.title,
+        eventLocation: finalData.location,
         eventDescription: finalData.description,
         eventLink: finalData.registration_link,
         eventDate: date,
@@ -226,25 +242,25 @@ const CreateEvent = () => {
       guest_designation: eventDetails.tagLine,
       guest_image: eventDetails.organizerImage,
       title: eventDetails.eventName,
-      location : eventDetails.eventLocation,
+      location: eventDetails.eventLocation,
       registration_link: eventDetails.eventLink,
       banner_image: eventDetails.eventImage,
       description: eventDetails.eventDescription,
       event_time: formatTime()
     };
-    if(validateForm(eventDetails , setEventDetailsError)){
-    try {
-      setIsLoading(true)
-      const data = await EventService.createNewEvent(userCookie, eventData);
-      if(data){
-        setIsLoading(false)
-        notify(data.data.message);
-        router.push('/admin/events')
+    if (validateForm(eventDetails, setEventDetailsError)) {
+      try {
+        setIsLoading(true);
+        const data = await EventService.createNewEvent(userCookie, eventData);
+        if (data) {
+          setIsLoading(false);
+          notify(data.data.message);
+          router.push("/admin/events");
+        }
+      } catch (err) {
+        notify(err?.response?.data?.message ?? err?.message, "error");
       }
-    } catch (err) {
-      notify(err?.response?.data?.message ?? err?.message, "error");
     }
-  }
   };
 
   const createUpdateHandler = async (e) => {
@@ -259,12 +275,9 @@ const CreateEvent = () => {
       event_time: formatTime()
     };
     try {
-      const data = await EventService.updateEvent(
-        eventID,
-        eventData,
-      );
+      const data = await EventService.updateEvent(eventID, eventData);
       notify(data.data.message);
-      router.push('/admin/events')
+      router.push("/admin/events");
     } catch (err) {
       notify(err?.response?.data?.message ?? err?.message, "error");
     }
@@ -274,18 +287,18 @@ const CreateEvent = () => {
   const clearEventHandler = (e) => {
     const clearEventDetails = {
       organizerImage: "",
-    organizerName: "",
-    tagLine: "",
-    eventName: "",
-    eventLocation: "",
-    eventDescription: "",
-    eventLink: "",
-    eventDate: "",
-    eventTime: "",
-    eventImage: ""
-    }
-    setEventDetails(clearEventDetails)
-  }
+      organizerName: "",
+      tagLine: "",
+      eventName: "",
+      eventLocation: "",
+      eventDescription: "",
+      eventLink: "",
+      eventDate: "",
+      eventTime: "",
+      eventImage: ""
+    };
+    setEventDetails(clearEventDetails);
+  };
 
   return (
     <div className="bg-gray-100 min-h-full pb-5">
@@ -305,27 +318,32 @@ const CreateEvent = () => {
         <OrganizerInfo
           eventDetails={eventDetails}
           setEventDetails={setEventDetails}
-          eventDetailsError = {eventDetailsError}
-          setEventDetailsError = {setEventDetailsError}
+          eventDetailsError={eventDetailsError}
+          setEventDetailsError={setEventDetailsError}
         />
         <EventInfo
           eventDetails={eventDetails}
           setEventDetails={setEventDetails}
-          eventDetailsError = {eventDetailsError}
-          setEventDetailsError = {setEventDetailsError}
+          eventDetailsError={eventDetailsError}
+          setEventDetailsError={setEventDetailsError}
         />
         <div className="flex items-center justify-end mx-10 mb-6">
-          <button className="border-2 border-black bg-white px-8 py-2 rounded mr-5" onClick = {(e) => clearEventHandler(e)}>
+          <button
+            className="border-2 border-black bg-white px-8 py-2 rounded mr-5"
+            onClick={(e) => clearEventHandler(e)}
+          >
             Cancel
           </button>
           <button
-            className={`border-2 flex items-center border-black bg-black px-8 py-2 rounded text-white ${isLoading && "opacity-50 cursor-not-allowed"}`}
+            className={`border-2 flex items-center border-black bg-black px-8 py-2 rounded text-white ${
+              isLoading && "opacity-50 cursor-not-allowed"
+            }`}
             onClick={(e) =>
               eventID ? createUpdateHandler(e) : createEventHandler(e)
             }
             type="button"
           >
-             {isLoading && <LoadIcon color="#fff" height="23px" />}
+            {isLoading && <LoadIcon color="#fff" height="23px" />}
             {eventID ? "Update" : "Create"}
           </button>
         </div>
