d9a9de52353702410e7ffb9c174b340ad496a6a2
Gowri Sankar D S <63556719+sNkr-10@users.noreply.github.com>
2021-12-08 21:53:05 +0530

    Revert "Revert "643 fix change photo not working""

--- component/admin/OrganizerInfo.jsx
+++ component/admin/OrganizerInfo.jsx
@@ -41,10 +41,11 @@ const OrganizerInfo = ({ eventDetails, setEventDetails ,  eventDetailsError , se
               eventDetails.organizerImage ? eventDetails.organizerImage.name ? image : eventDetails.organizerImage 
                 : "/images/svgs/avatar.svg"
             }`}
+            onClick = {() => ref.current.click()}
             alt="profile image"
             height={180}
             width={180}
-            className="cursor-pointer rounded-full"
+            className="cursor-pointer rounded-full filter hover:brightness-75"
           />
           <div onClick={() => ref.current.click()}>
             <label htmlFor="inputFiles" className="font-bold relative pl-8">
