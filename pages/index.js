025398788bb6fb77644afdd30a6c7d0472c1af93
jasirneoito <jasir@neoito.com>
2021-12-20 23:49:31 +0530

    fix: verify email api connected #695
    added email varification api

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,6 @@ export async function getServerSideProps(context) {
     };
     const responsePost = await PostService.getPostsByUsers(postParams);
     const { data } = await UserService.getLatestUsers(userParams);
-    // console.log(data.users, 'd' );
     const eventParams = {
       sort_field: "event_time",
       order: "ASC",
@@ -52,14 +51,12 @@ export async function getServerSideProps(context) {
       }
     };
   } catch (err) {
-    console.log(err);
     notify(err?.response?.data?.message ?? err?.message, "error");
     return { props: { posts: [], events: [], user: []} };
   }
 }
 //addCourses()
 export default function Home({ posts, user, events }) {
-  console.log(events, 'error');
   return (
     <div className="wrap">
       <Head>
