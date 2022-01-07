11e8fb528e84a05009ce36f97a31ecafccc6a498
jasirneoito <jasir@neoito.com>
2022-01-07 13:36:08 +0530

    fix: added solution in next.config #741
    added solution code in next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 const path = require("path");
 
+// Nextjs 404s on buildManifest across multiple EC2 instances
+//https://stackoverflow.com/questions/63817674/nextjs-404s-on-buildmanifest-across-multiple-ec2-instances
+
+const execSync = require("child_process").execSync;
+
+const lastCommitCommand = "git rev-parse HEAD";
+
+module.exports = {
+  async generateBuildId() {
+    return execSync(lastCommitCommand).toString().trim();
+  },
+};
+
 module.exports = {
   productionBrowserSourceMaps: process.env.GENERATE_SOURCEMAP,
   images: {
@@ -14,6 +27,7 @@ module.exports = {
     };
     return config;
   },
+  
   env: {
     BASE_URL: process.env.BASE_URL,
     CLIENT_URL: process.env.CLIENT_URL,
