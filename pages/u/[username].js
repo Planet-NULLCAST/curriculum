06fccf0585e18f8c2ff2f812f1e7602ee247aee1
jasirneoito <jasir@neoito.com>
2022-01-17 14:21:57 +0530

    fix: logout issue #509
    fixed logout issue

--- pages/u/[username].js
+++ pages/u/[username].js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
-
 import Navbar from "../../component/profile/Navbar";
 import { getCookieValue } from "../../lib/cookie";
 import Activity from "../../component/profile/Activity";
@@ -12,10 +11,8 @@ import LuckEgg from "../../component/profile/LuckEgg";
 import SiteHeader from "../../component/layout/SiteHeader/SiteHeader";
 import UserService from "../../services/UserService";
 import PostService from "../../services/PostService";
-import Cookies from "universal-cookie";
 import Profilestyles from "../../styles/Profile.module.css";
 import SkillSet from "../../component/profile/SkillSet";
-import notify from "../../lib/notify";
 
 export async function getServerSideProps(context) {
   try {
@@ -29,7 +26,6 @@ export async function getServerSideProps(context) {
         const userId = cookie.id;
         const username = context.params.username;
 
-        // let isThisUserTheCurrentLoggedIn = false;
         const data = await UserService.getUserByUsername(username);
         return {
           props: {
@@ -38,18 +34,22 @@ export async function getServerSideProps(context) {
           }
         };
       } else {
+        //user logout from user profile
+        const data = await UserService.getUserByUsername(context.params.username);
         return {
-          redirect: {
-            permanent: false,
-            destination: "/404"
-          }
+           props : {
+              userData : data?.data,
+              userCurrentLogin: 0
+           }
         };
       }
     } else {
       const data = await UserService.getUserByUsername(context.params.username);
       return {
          props : {
-            userData : data?.data
+            userData : data?.data,
+            userCurrentLogin: data.data.id
+
          }
       };
     }
@@ -66,6 +66,7 @@ export async function getServerSideProps(context) {
 }
 
 export default function Username({ userData, userCurrentLogin }) {
+  console.log(userCurrentLogin);
   const [currentNav, setCurrentNav] = useState("profile");
   const [newBlogs, setNewBlogs] = useState();
   const [postsCount, setPostsCount] = useState();
