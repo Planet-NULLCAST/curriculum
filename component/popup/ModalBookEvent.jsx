0661c17816fc5b8c10457fe3af8a8428f7ba9fa8
jasirneoito <jasir@neoito.com>
2022-01-27 22:08:25 +0530

    fix: book event  #797
    fixed

--- component/popup/ModalBookEvent.jsx
+++ component/popup/ModalBookEvent.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 import styles from "./ModalBookEvent.module.scss";
-import img from "../../public/images/svgs/eventbookconfirm.svg"
 const ModalBookEvent = ({
   open,
   setOpen,
@@ -12,13 +11,12 @@ const ModalBookEvent = ({
     <Popup open={open} modal nested>
       {(close) => (
         <div className="modal flex justify-center items-center rounded-xl bg-white relative">
-
           <div className={styles.card}>
             <div className={styles.close}>
               <i className="fas fa-times"></i>
             </div>
             <div className={styles.body}>
-              <img src={img} className={styles.image} alt="" />
+              <img src="/images/svgs/bookSuccess.svg" className={styles.image} alt="" />
               <div className={styles.text1}>You’re registered for the event</div>
               <div className={styles.text2}>You can check your email for further information </div>
               <div className={styles.btnbox}>
