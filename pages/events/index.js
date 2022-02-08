119f0c9994b5f61413c6aee4fcb2333036562664
Rishabh Kunwar <rishabhkunwar11@gmail.com>
2022-02-08 14:43:32 +0530

    fix: minor changes

--- pages/events/index.js
+++ pages/events/index.js
@@ -24,13 +24,12 @@ export async function getServerSideProps() {
       with_table: "users, tags"
     };
     const responseEvents = await EventService.getLatestEvents(postParams);
-    console.log(responseEvents?.count , "here");
-    if (responseEvents?.count > 0) {
+    if (responseEvents.data.count > 0) {
       return {
         props: {
-          events: responseEvents.events,
-          count: responseEvents.count,
-          limit: responseEvents.limit
+          events: responseEvents.data.events,
+          count: responseEvents.data.count,
+          limit: responseEvents.data.limit
         }
       };
     } else {
@@ -43,7 +42,6 @@ export async function getServerSideProps() {
       };
     }
   } catch (err) {
-    console.log(err , "error");
     notify(err?.response?.data?.message ?? err?.message, "error");
     return {
       props: {}
@@ -53,7 +51,7 @@ export async function getServerSideProps() {
 
 export default function EventListing({ events, count, limit }) {
   const [newEvents, setNewEvents] = useState([]);
-  console.log(events);
+
   const currentCount = (count) => {
     getNewEvents(count);
   };
@@ -66,11 +64,11 @@ export default function EventListing({ events, count, limit }) {
   const eventsHandler = (events) => {
     var today = new Date();
     const CurrentDateTime = today.toISOString();
-    const eventsUpdated = events?.filter(function (event) {
+
+    const eventsUpdated = events.filter(function (event) {
       console.log(event.event_time > CurrentDateTime);
       return event.event_time > CurrentDateTime;
     });
-    console.log(eventsUpdated);
     setNewEvents(eventsUpdated);
   };
 
@@ -136,7 +134,7 @@ export default function EventListing({ events, count, limit }) {
       {newEvents && <EventFeatured event={newEvents[0] || ""} />}
       {newEvents && events?.length > 0 ? (
         <EvListing
-          events={events?.filter((each) => each.id !== newEvents[0]?.id) || ""}
+          events={events.filter((each) => each.id !== newEvents[0]?.id) || ""}
           currentCount={currentCount}
           eventCount={newEvents.count}
         />
